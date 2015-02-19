0cdea90c8ddf69d13a33650d2f9907e10727eaee
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-02-19 09:38:18 +0100

    Week 5: prefix-sum kernel (draft — not supporting arbitrary size)

--- list-scan/main.cpp
+++ list-scan/main.cpp
@@ -1,7 +1,5 @@
 #include    <wb.h>
 
-#define BLOCK_SIZE 512 //@@ You can change this
-
 /**
  * Uses code made available on webgpu.com
  * by the Heterogeneous Parallel Programming MOOC teaching team
@@ -10,6 +8,8 @@
 // MP Scan
 // Given a list (lst) of length n
 // Output its prefix sum = {lst[0], lst[0] + lst[1], lst[0] + lst[1] + ... + lst[n-1]}
+
+#define BLOCK_SIZE 512 //@@ You can change this
 
 #define wbCheck(stmt) do {                                                    \
         cudaError_t err = stmt;                                               \
@@ -20,11 +20,55 @@
         }                                                                     \
     } while(0)
 
-__global__ void scan(float * input, float * output, int len) {
-    //@@ Modify the body of this function to complete the functionality of
-    //@@ the scan on the device
-    //@@ You may need multiple kernel calls; write your kernels before this
-    //@@ function and call them from here
+__device__ void listReduction(float * local) {
+    // For each level of the reduction tree
+    for(int stride = 1; stride <= BLOCK_SIZE; stride *= 2) {
+        int index = (threadIdx.x + 1) * stride * 2 - 1;
+
+        // At each step, half the threads go unused. Note we keep alive
+        // adjacent threads so as to minimize control divergence in warps.
+        if(index < BLOCK_SIZE * 2) {
+            local[index] = local[index] + local[index - stride];
+        }
+        __syncthreads();
+    }
+}
+
+__device__ void postProcessing(float * local) {
+    for(int stride = BLOCK_SIZE / 2; stride >= 1; stride /= 2) {
+        int index = (threadIdx.x + 1) * stride * 2 - 1;
+
+        // At each step, twice as many threads go to use
+        if(index + stride < BLOCK_SIZE * 2) {
+            local[index + stride] = local[index + stride] + local[index];
+        }
+        __syncthreads();
+    }
+}
+
+__global__ void scan(float * input, float * output, float * offsets, int len) {
+    __shared__ float local[BLOCK_SIZE * 2];
+
+    // Collaborative loading of the input vector (two element per thread)
+    unsigned int tx = threadIdx.x,
+                 offset = (blockIdx.x * BLOCK_SIZE + tx) * 2;
+    local[tx * 2] = (offset < len ? input[offset] : 0);
+    local[tx * 2 + 1] = (offset + 1 < len ? input[offset + 1] : 0);
+    __syncthreads();
+
+    // Phase 1: reduction tree (similar to a simple list reduction)
+    listReduction(local);
+
+    // Phase 2: post-processing
+    // After execution, only some entries of `local` contain the correct
+    // partial sum. We need to complete the missing ones.
+    postProcessing(local);
+
+    // Output
+    int ox = blockIdx.x * BLOCK_SIZE + tx;
+    if(ox < len) {
+        output[ox] = local[tx];
+    }
 }
 
 int main(int argc, char ** argv) {
@@ -32,24 +76,29 @@ int main(int argc, char ** argv) {
     float * hostInput; // The input 1D list
     float * hostOutput; // The output list
     float * deviceInput;
+    float * deviceOffsets;
     float * deviceOutput;
     int numElements; // number of elements in the list
+    int numBlocks;
 
     args = wbArg_read(argc, argv);
 
     wbTime_start(Generic, "Importing data and creating memory on host");
     hostInput = (float *) wbImport(wbArg_getInputFile(args, 0), &numElements);
     hostOutput = (float*) malloc(numElements * sizeof(float));
+    numBlocks = (numElements - 1) / BLOCK_SIZE + 1;
     wbTime_stop(Generic, "Importing data and creating memory on host");
 
     wbLog(TRACE, "The number of input elements in the input is ", numElements);
 
     wbTime_start(GPU, "Allocating GPU memory.");
     wbCheck(cudaMalloc((void**)&deviceInput, numElements*sizeof(float)));
+    wbCheck(cudaMalloc((void**)&deviceOffsets, numBlocks*sizeof(float)));
     wbCheck(cudaMalloc((void**)&deviceOutput, numElements*sizeof(float)));
     wbTime_stop(GPU, "Allocating GPU memory.");
 
     wbTime_start(GPU, "Clearing output memory.");
+    // Set all output elements to zero (default value)
     wbCheck(cudaMemset(deviceOutput, 0, numElements*sizeof(float)));
     wbTime_stop(GPU, "Clearing output memory.");
 
@@ -58,11 +107,10 @@ int main(int argc, char ** argv) {
     wbTime_stop(GPU, "Copying input memory to the GPU.");
 
     //@@ Initialize the grid and block dimensions here
-
+    dim3 gridSize(numBlocks, 1, 1);
+    dim3 blockSize(BLOCK_SIZE, 1, 1);
     wbTime_start(Compute, "Performing CUDA computation");
-    //@@ Modify this to complete the functionality of the scan
-    //@@ on the deivce
-
+    scan<<<gridSize, blockSize>>>(deviceInput, deviceOutput, deviceOffsets, numElements);
     cudaDeviceSynchronize();
     wbTime_stop(Compute, "Performing CUDA computation");
 
@@ -82,4 +130,3 @@ int main(int argc, char ** argv) {
 
     return 0;
 }
-
